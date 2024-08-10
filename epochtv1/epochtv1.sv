ac144e16c0c751cf839609f974783eac0ed4aa0f
David Hunter <reverendgumby@gmail.com>
2024-08-10 15:59:18 -0700

    Sprite pipeline: Replace with a clock-driven fill pipeline

--- epochtv1/epochtv1.sv
+++ epochtv1/epochtv1.sv
@@ -64,9 +64,12 @@ localparam [9:0] LAST_COL_RENDER = 9'd28 + 9'd192 - 1'd1;
 localparam [9:0] FIRST_COL_HSYNC = 9'd0;
 localparam [9:0] LAST_COL_HSYNC = 9'd19;
 
+localparam [9:0] FIRST_ROW_PRE_RENDER = FIRST_ROW_RENDER - 'd2;
+
 
 reg [8:0]    row, col;
 reg          field;
+wire         pre_render_row;
 wire         render_row, render_col;
 
 
@@ -129,6 +132,41 @@ typedef struct packed
 
 s_objattr oam [128];
 
+s_objattr  oam_rbuf;
+always_ff @(posedge CLK) begin
+  oam_rbuf <= oam[spr_oam_idx];
+end
+
+
+//////////////////////////////////////////////////////////////////////
+// Object Line Buffer (OLB)
+// - 8 pixels wide to enable writing 1/2 sprite in one cycle
+// - pixel = 4 bit color + 1 bit opaque
+// - two full rows, used in ping-pong fashion
+// - olb_rc: clears after read, to prepare for next row
+
+wire [5:0]  olb_wa;
+wire [39:0] olb_wd;
+reg [7:0]   olb_we;
+wire [5:0]  olb_ra;
+reg [39:0]  olb_rd;
+wire        olb_rc;
+
+reg [39:0]  olb_mem [64];
+
+always_ff @(posedge CLK) if (CE) begin
+  for (int i = 0; i < 8; i++) begin
+    if (olb_we[i])
+      olb_mem[olb_wa][(i*5)+:5] <= olb_wd[(i*5)+:5];
+  end
+end
+
+always_ff @(posedge CLK) if (CE) begin
+  olb_rd <= olb_mem[olb_ra];
+  if (olb_rc)
+    olb_mem[olb_ra] <= 0;
+end
+
 
 //////////////////////////////////////////////////////////////////////
 // Sprite pipeline
@@ -137,61 +175,135 @@ s_objattr oam [128];
 
 reg [15:0] vram [2048];
 
-// Object Line Buffer (OLB)
-reg [4:0]  olb [256];
+enum reg [2:0]
+{
+ SST_IDLE,
+ SST_EVAL,
+ SST_DRAW_L,
+ SST_DRAW_R
+} spr_st;
 
-reg [7:0]  spr_x;
+reg [6:0] spr_oam_idx;
 
-// TODO: Replace with a clock-driven fill pipeline.
-task fill_olb;
-integer i, j;
-reg [7:0] spr_y0;
-reg [3:0] spr_y;
-reg [7:0] x;
+reg spr_olb_wsel;
+assign spr_olb_wsel = ~row[0];
+
 reg [15:0] pat;
+reg [11:0] spr_vram_addr;
+reg [7:0]  spr_y0;
+reg [3:0]  spr_y;
+reg        spr_dr;              // sprite left/right side
+
+always_ff @(posedge CLK) begin
+  pat <= vram[spr_vram_addr];
+end
+assign spr_vram_addr = {oam_rbuf.pat, spr_y[3:1], spr_dr};
+assign spr_y0 = oam_rbuf.y*2 - 1'd1;
+assign spr_y = row - spr_y0;
+assign spr_dr = spr_st == SST_DRAW_R;
+
+always_ff @(posedge CLK) if (CE) begin
+integer i, j;
+reg [7:0] x;
 reg px;
-s_objattr oa;
-  for (i = 0; i < 256; i++) begin
-    olb[i] = 0;
+  if (~(pre_render_row | render_row)) begin
+    spr_st <= SST_IDLE;
   end
-  for (i = 0; i < 128; i++) begin
-    oa = oam[i];
-    spr_y0 = oa.y*2;
-    if (row >= spr_y0 && row <= spr_y0 + 15) begin
-      spr_y = row - spr_y0;
-      for (j = 0; j < 16; j++) begin
-        x = oa.x*2 + j;
-        pat = vram[{oa.pat, spr_y[3:1], j[3]}];
-        px = pat[4'd15 - {~j[2], spr_y[0], j[1:0]}];
-        if (px)
-          olb[x] = {1'b1, oa.color};
-      end
-    end
-  end
-endtask
-
-always @row
-  fill_olb();
-
-reg [4:0] spr_px;
-
-assign spr_x = col[7:0];
-
-always @(posedge CLK) if (CE) begin
-  if (~render_row) begin
-    spr_px <= 0;
+  else if (col == 0) begin
+    spr_oam_idx <= 0;
+    spr_st <= SST_EVAL;
   end
   else begin
-    spr_px <= olb[spr_x];
+    if (spr_st == SST_EVAL) begin
+      spr_st <= SST_EVAL;
+    end
+    if (spr_st == SST_EVAL) begin
+      if (row >= spr_y0 && row <= spr_y0 + 15) begin
+        spr_st <= SST_DRAW_L;
+      end
+      else begin
+        spr_st <= (spr_oam_idx < 7'd127) ? SST_EVAL : SST_IDLE;
+        spr_oam_idx <= spr_oam_idx + 1'd1;
+      end
+    end
+    else if (spr_st == SST_DRAW_L) begin
+      spr_st <= SST_DRAW_R;
+    end
+    else if (spr_st == SST_DRAW_R) begin
+      spr_st <= (spr_oam_idx < 7'd127) ? SST_EVAL : SST_IDLE;
+      spr_oam_idx <= spr_oam_idx + 1'd1;
+    end
   end
 end
+
+reg        spr_dact;
+reg [7:0]  spr_dpat;
+reg        spr_dact_d, spr_dact_d2;
+reg [15:0] spr_dsr;             // draw shift register
+reg [7:0]  spr_dsx;             // current drawing column
+reg [3:0]  spr_dclr;            // current sprite color
+
+always @* begin
+  spr_dpat = 0;
+  spr_dact = 0;
+  if ((spr_st == SST_DRAW_L) | (spr_st == SST_DRAW_R)) begin
+    spr_dact = 1'b1;
+    for (int i = 0; i < 8; i++) begin
+      spr_dpat[i] = pat[4'd15 - {~i[2], spr_y[0], i[1:0]}];
+    end
+  end
+end
+
+always_ff @(posedge CLK) if (CE) begin
+  spr_dsr <= {spr_dpat, spr_dsr[15:8]};
+
+  spr_dact_d <= spr_dact;
+  if (spr_st == SST_DRAW_L) begin
+    spr_dsx <= oam_rbuf.x*2;
+    spr_dclr <= oam_rbuf.color;
+  end
+  else if (spr_dact_d) begin
+    spr_dsx <= spr_dsx + 8'd8;
+  end
+
+  spr_dact_d2 <= spr_dact_d;
+end
+
+assign olb_wa = {spr_olb_wsel, spr_dsx[7:3]};
+assign olb_wd = {8{1'b1, spr_dclr}};
+
+always @* begin
+  olb_we = 0;
+  if (spr_dact_d | spr_dact_d2) begin
+    for (int i = 0; i < 8; i++) begin
+    reg [4:0] p;
+      p = 5'd8 + i[4:0] - spr_dsx[2:0];
+      olb_we[i[2:0]] = spr_dsr[p];
+    end
+  end
+end
+
+wire [7:0] spr_olb_rx;
+reg [7:0]  spr_olb_rrs;
+wire       spr_olb_rsel;
+wire [4:0] spr_px;
+
+assign spr_olb_rsel = row[0];
+assign spr_olb_rx = col[7:0];
+
+assign olb_ra = {spr_olb_rsel, spr_olb_rx[7:3]};
+assign olb_rc = &spr_olb_rx[2:0];
+
+always_ff @(posedge CLK) if (CE) begin
+  spr_olb_rrs <= spr_olb_rx[2:0];
+end
+
+assign spr_px = olb_rd[(spr_olb_rrs*5)+:5];
 
 
 //////////////////////////////////////////////////////////////////////
 // Sync generator
 
-assign render_row = (row >= FIRST_ROW_RENDER) & (row <= LAST_ROW_RENDER);
-assign render_col = (col >= FIRST_COL_RENDER) & (col <= LAST_COL_RENDER);
 reg  de, hsync, vsync, vbl;
 
 always_ff @(posedge CLK) if (CE) begin
@@ -211,6 +323,11 @@ assign VS = vsync;
 // Render pipeline
 
 wire [3:0] pd = spr_px[4] ? spr_px[3:0] : 0;
+
+assign pre_render_row = (row >= FIRST_ROW_PRE_RENDER) & (row < FIRST_ROW_RENDER);
+
+assign render_row = (row >= FIRST_ROW_RENDER) & (row <= LAST_ROW_RENDER);
+assign render_col = (col >= FIRST_COL_RENDER) & (col <= LAST_COL_RENDER);
 
 
 //////////////////////////////////////////////////////////////////////
