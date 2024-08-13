a065c2266fd5cb1e230c045d72322bd1c0548e9f
David Hunter <reverendgumby@gmail.com>
2024-08-13 08:29:36 -0700

    Integrate SCV core
    It boots and runs the built-in VIDEO GAME TEST DISPLAY.

--- scv.sv
+++ scv.sv
@@ -209,24 +209,7 @@ localparam CONF_STR = {
 	"SuperCassetteVision;;",
 	"-;",
 	"O[122:121],Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
-	"O[2],TV Mode,NTSC,PAL;",
-	"O[4:3],Noise,White,Red,Green,Blue;",
-	"-;",
-	"P1,Test Page 1;",
-	"P1-;",
-	"P1-, -= Options in page 1 =-;",
-	"P1-;",
-	"P1O[5],Option 1-1,Off,On;",
-	"d0P1F1,BIN;",
-	"H0P1O[10],Option 1-2,Off,On;",
-	"-;",
-	"P2,Test Page 2;",
-	"P2-;",
-	"P2-, -= Options in page 2 =-;",
-	"P2-;",
-	"P2S0,DSK;",
-	"P2O[7:6],Option 2,1,2,3,4;",
-	"-;",
+	//"O[2],TV Mode,NTSC,PAL;",
 	"-;",
 	"T[0],Reset;",
 	"R[0],Reset and close OSD;",
@@ -269,42 +252,19 @@ pll pll
 
 wire reset = RESET | status[0] | buttons[1];
 
-wire [1:0] col = status[4:3];
-
-wire HBlank;
-wire HSync;
-wire VBlank;
-wire VSync;
-wire ce_pix;
-wire [7:0] video;
-
-mycore mycore
+scv scv
 (
-	.clk(clk_sys),
-	.reset(reset),
+	.CLK(clk_sys),
+	.RESB(~reset),
 	
-	.pal(status[2]),
-	.scandouble(forced_scandoubler),
-
-	.ce_pix(ce_pix),
-
-	.HBlank(HBlank),
-	.HSync(HSync),
-	.VBlank(VBlank),
-	.VSync(VSync),
-
-	.video(video)
+	.VID_PCE(CE_PIXEL),
+	.VID_DE(VGA_DE),
+	.VID_HS(VGA_HS),
+	.VID_VS(VGA_VS),
+	.VID_RGB({VGA_R, VGA_G, VGA_B})
 );
 
 assign CLK_VIDEO = clk_sys;
-assign CE_PIXEL = ce_pix;
-
-assign VGA_DE = ~(HBlank | VBlank);
-assign VGA_HS = HSync;
-assign VGA_VS = VSync;
-assign VGA_G  = (!col || col == 2) ? video : 8'd0;
-assign VGA_R  = (!col || col == 1) ? video : 8'd0;
-assign VGA_B  = (!col || col == 3) ? video : 8'd0;
 
 reg  [26:0] act_cnt;
 always @(posedge clk_sys) act_cnt <= act_cnt + 1'd1; 
